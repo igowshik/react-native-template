e7f5cbe54661162a73d48f4239e923165c502d27
Selvam K <selvamk@cnxsi.com>
2019-08-16 12:44:57 +0530

    Setup hadle step 1

--- android/app/src/main/java/com/cnxapp/MainApplication.java
+++ android/app/src/main/java/com/cnxapp/MainApplication.java
@@ -32,20 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new BlurViewPackage(),
-            new CameraRollPackage(),
-            new RNFSPackage(),
-            new RNCameraPackage(),
-            new ReanimatedPackage(),
-            new AsyncStoragePackage(),
-            new RNCWebViewPackage(),
-            new VectorIconsPackage(),
-            new SvgPackage(),
-            new LinearGradientPackage(),
-            new RNGestureHandlerPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage());
     }
 
     @Override
